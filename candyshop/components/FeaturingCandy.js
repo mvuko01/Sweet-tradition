270b9ee820e1f696e4475717d420287132b38f8d
mvuko01 <mvuko01@fesb.hr>
2023-01-06 23:28:03 +0100

    Added whats trending component without responsivnes

--- candyshop/components/FeaturingCandy.js
+++ candyshop/components/FeaturingCandy.js
@@ -6,26 +6,17 @@ const FeaturingCandy = () => {
         <>
         <section className={styles.product}>
             <h1 className={styles.featuringHeading}>WHAT'S TRENDING</h1>
-            <button className= {styles.buttonPrevious}>
-                <Image
-                    src={'/Arrow.svg'}
-                    alt=""
-                    width={100}
-                    height={100}
-                    className={styles.imageArrow}
-                />
-            </button>
-            <button className= {styles.buttonNext}>
-                <Image
-                    src={'/Arrow.svg'}
-                    alt=""
-                    width={100}
-                    height={100}
-                    className={styles.imageArrow}
-
-                />
-            </button>
+            
             <div className ={styles.productContainer}>
+                <button className= {styles.buttonPrevious}>
+                    <Image
+                        src={'/Arrow.svg'}
+                        alt=""
+                        width={100}
+                        height={100}
+                        className={styles.imageArrow}
+                    />
+                </button>
                 <div className = {styles.productCard}>
                     <div className = {styles.productUpper}>
                         <button className= {styles.buttonFavourite}>
@@ -33,6 +24,38 @@ const FeaturingCandy = () => {
                                 src={'/EmptyHeart.svg'}
                                 width={100}
                                 height={100}
+                                className ={styles.imageFavourite}
+                            />
+                        </button>
+                        <Image
+                            src={'/MaynardWineGums.svg'}
+                            width={100}
+                            height={100}
+                            className = {styles.imageProduct}
+                        />
+                    </div>
+                    <div className={styles.productInfo}>
+                        <p className={styles.productDescription}>Chewy sweets, 195g</p>
+                        <h2 className={styles.productName}>Maynard wine gums</h2>
+                        <span className={styles.productPrice}>7,50€</span>
+                        <button className={styles.buttonCart}>
+                            <Image
+                                src={'/Add to cart.svg'}
+                                width={100}
+                                height={100}
+                                className ={styles.imageCart}
+                            />
+                        </button>
+                    </div>
+                </div>
+                <div className = {styles.mainProductCard}>
+                    <div className = {styles.productUpper}>
+                        <button className= {styles.buttonFavourite}>
+                            <Image
+                                src={'/EmptyHeart.svg'}
+                                width={100}
+                                height={100}
+                                className ={styles.imageFavourite}
                             />
                         </button>
                         <Image
@@ -43,18 +66,60 @@ const FeaturingCandy = () => {
                         />
                     </div>
                     <div className={styles.productInfo}>
-                        <p className={styles.productDescription}>Chewy sweets, 125g</p>
-                        <h2 className={styles.productName}>Werthers originals</h2>
-                        <span className={styles.productPrice}>2,50€</span>
+                        <p className={styles.mainProductDescription}>Chewy sweets, 125g</p>
+                        <h2 className={styles.mainProductName}>Bilar swedish candy</h2>
+                        <span className={styles.mainProductPrice}>2,50€</span>
                         <button className={styles.buttonCart}>
                             <Image
                                 src={'/Add to cart.svg'}
                                 width={100}
                                 height={100}
+                                className ={styles.imageCart}
                             />
                         </button>
                     </div>
                 </div>
+                <div className = {styles.productCard}>
+                    <div className = {styles.productUpper}>
+                        <button className= {styles.buttonFavourite}>
+                            <Image
+                                src={'/EmptyHeart.svg'}
+                                width={100}
+                                height={100}
+                                className ={styles.imageFavourite}
+                            />
+                        </button>
+                        <Image
+                            src={'/BassetsMurrayMints.svg'}
+                            width={100}
+                            height={100}
+                            className = {styles.imageProduct}
+                        />
+                    </div>
+                    <div className={styles.productInfo}>
+                        <p className={styles.productDescription}>Mints, 193g</p>
+                        <h2 className={styles.productName}>Bassett's Murray Mints</h2>
+                        <span className={styles.productPrice}>7,50€</span>
+                        <button className={styles.buttonCart}>
+                            <Image
+                                src={'/Add to cart.svg'}
+                                width={100}
+                                height={100}
+                                className ={styles.imageCart}
+                            />
+                        </button>
+                    </div>
+                </div>
+                <button className= {styles.buttonNext}>
+                    <Image
+                        src={'/Arrow.svg'}
+                        alt=""
+                        width={100}
+                        height={100}
+                        className={styles.imageArrow}
+
+                    />
+                </button>
             </div>
         </section>
         
