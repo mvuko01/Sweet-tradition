4197fa831421beb1210bfe0a88cfc8a508244c25
“kbtomic” <klara.tomic69@gmail.com>
2023-01-14 22:20:33 +0100

    Added addNewBlog page

--- candyshop/pages/blog/addNewBlog.js
+++ candyshop/pages/blog/addNewBlog.js
@@ -4,8 +4,10 @@ import useAuth from "../../hooks/useAuth";
 import api from "../../api";
 import { useEffect, useState } from 'react';
 import NotAuthorized from "../../components/NotAuthorized";
+import styles from '../../styles/NewBlog.module.css'
+import Image from 'next/image'
 
-const AddNew = () => {
+const AddNew = ({images}) => {
     const { token } = useAuth();
     const [currentUser, setCurrentUser] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -32,8 +34,54 @@ const AddNew = () => {
     return (
         <>
         <Header />
+        <div className={styles.mainWrapper}>
+        <p className={styles.title} >Add new Blog</p>
+        <p className={styles.adviceTitle}>ADVICE FROM OUR TEAM</p>
+        <p className={styles.advice}>1. Don&#39;t use offensive language.</p>
+        <p className={styles.advice}>2. Don&#39;t post personal information or the personal information of others.</p>
+        <p className={styles.advice}>3. Respect the opinions of others.</p>
+        <p className={styles.advice}>4. Be aware of copyright laws.</p>
+        <p className={styles.advice}>5. Don&#39;t post anything that could be interpreted as libelous.</p>
+        <p className={styles.advice}>6. Don&#39;t post anything that could be seen as promoting illegal activities.</p>
+        <p className={styles.advice}>7. Don&#39;t post anything that could be seen as promoting hate speech.</p>
+        <input placeholder='Insert heading...' className={styles.heading}></input>
+        <div className={styles.uploadWrapper}>
+            <p className={styles.uploadImage}>Upload image</p>
+        </div>
+        <p className={styles.titleText}>Text</p>
+        <div className={styles.textMainWrapper}>
+            <div className={styles.textEditWrapper}>
+                {images.map((image) => (
+                    <Image key={image}
+                    src={`/newBlog/${image}`}
+                    alt="Edit text"
+                    className={styles.editIcon}
+                    width={30}
+                    height={30}
+                  />
+                ))}
+            </div>
+            <div className={styles.textWrapper}>
+                <input className={styles.writeText} placeholder="Write text..."></input>
+            </div>
+        </div>
+        <div className={styles.btnWrapper}>
+            <button type='button' className={styles.publish}>PUBLISH</button>
+        </div>
+        </div> 
         <Footer />
         </>
     );
 };
 export default AddNew;
+
+import fs from 'fs'
+export async function getStaticProps() {
+    const images = fs.readdirSync('public/newBlog');
+
+    return {
+        props: {
+            images,
+        },
+    };
+}
