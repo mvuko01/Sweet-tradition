b2d84bdb5d09a55704d2916c5ea5bb539b7adb6a
“kbtomic” <klara.tomic69@gmail.com>
2023-01-16 08:12:51 +0100

    Implemented shifting between product in featuring candy, need to fix bug when array is over

--- candyshop/components/FeaturingCandy.js
+++ candyshop/components/FeaturingCandy.js
@@ -2,8 +2,13 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import MainProductCard from './MainProductCard';
 import SideProductCard from './SideProductCard';
+import React, { useState } from 'react';
 
 const FeaturingCandy = ({products}) => {
+    console.log(products.length)
+    const centralIndex = 1;
+    const nextPicture = 1;
+    const [pic, setPic] = useState(centralIndex);
     return (
         <>
             <section className={styles.product}>
@@ -17,17 +22,25 @@ const FeaturingCandy = ({products}) => {
                             width={100}
                             height={100}
                             className={styles.imageArrow}
+                            onClick={() => pic == 1 ? setPic(products.length - 2) : setPic(pic - nextPicture)}
                         />
                     </button>
                     <div className={styles.productContainer}>
-                        {products.slice(0,3).map((product) => { 
+                        {/* {products.slice(0,3).map((product) => { 
                             if(product.frontmatter.id !== "1")                       
                                 return <SideProductCard key={product.frontmatter.id} name={product.frontmatter.name} short_description={`${product.frontmatter.category}, ${product.frontmatter.quantity}`} picture={product.frontmatter.picture} price={product.frontmatter.price} id={product.frontmatter.id} />
                             else
                                return <MainProductCard key={product.frontmatter.id} name={product.frontmatter.name} short_description={`${product.frontmatter.category}, ${product.frontmatter.quantity}`} picture={product.frontmatter.picture} price={product.frontmatter.price} id={product.frontmatter.id} />
-                        })}
-                        
-
+                        })} */}
+                            {products.slice(pic - 1, pic).map((product) => (
+                        <SideProductCard key={product.frontmatter.id} name={product.frontmatter.name} short_description={`${product.frontmatter.category}, ${product.frontmatter.quantity}`} picture={product.frontmatter.picture} price={product.frontmatter.price} id={product.frontmatter.id} />
+                    ))}
+                        {products.slice(pic, pic + 1).map((product) => (
+                            <MainProductCard key={product.frontmatter.id} name={product.frontmatter.name} short_description={`${product.frontmatter.category}, ${product.frontmatter.quantity}`} picture={product.frontmatter.picture} price={product.frontmatter.price} id={product.frontmatter.id} />
+                        ))}
+                        {products.slice(pic + 1, pic + 2).map((product) => (
+                        <SideProductCard key={product.frontmatter.id} name={product.frontmatter.name} short_description={`${product.frontmatter.category}, ${product.frontmatter.quantity}`} picture={product.frontmatter.picture} price={product.frontmatter.price} id={product.frontmatter.id} />
+                    ))}
                     </div>
                     <button className={styles.buttonNext}>
                         <Image
@@ -36,7 +49,7 @@ const FeaturingCandy = ({products}) => {
                             width={100}
                             height={100}
                             className={styles.imageArrow}
-
+                            onClick={() => pic <= products.length ? setPic(pic + nextPicture) : setPic(0)}
                         />
                     </button>
                 </div>
