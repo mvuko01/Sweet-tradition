2dec390f21522a641e4210fcf1e2a2cd616ed621
“kbtomic” <klara.tomic69@gmail.com>
2023-01-21 08:59:07 +0100

    Change font size on about us section for mobile

--- candyshop/components/AboutUs.js
+++ candyshop/components/AboutUs.js
@@ -17,7 +17,7 @@ const AboutUs = () => {
                     <p className={styles.text}>Candy shop sweet tradition is a one-stop destination for the sweet tooth. We offer a wide selection of premium-grade candy, from classic favorites like chocolate, gummies, and caramels to more unique and hard-to-find treats.</p>
                 </div>
                <div className={styles.secondWrapper}>
-                    <p className={styles.text}>Our commitment to quality and customer service has earned us a reputation for being the best candy shop around. Whether you&#39;re looking for something special for a birthday or just want to indulge your sweet tooth, you&#39;l find just what you&#39;re looking for in our candy shop.</p>
+                    <p className={styles.text}>Our commitment to quality and customer service has earned us a reputation for being the best candy shop around. Whether you&#39;re looking for something special for a birthday or just want to indulge your sweet tooth, you&#39;ll find just what you&#39;re looking for in our candy shop.</p>
                     <Image
                                     src={'/Image about 2.svg'}
                                     alt="About us image 2"
