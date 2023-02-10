f9da6d365e497617169f869f3fed393aa011ff7d
“kbtomic” <klara.tomic69@gmail.com>
2023-02-10 21:18:12 +0100

    Responsiveness of blog page

--- candyshop/pages/blog/blogs.js
+++ candyshop/pages/blog/blogs.js
@@ -51,42 +51,44 @@ const Blogs = (props) => {
                 alt="profile image"
                 className={styles.banner}
             />
-            <Link href="/blog/addNewBlog"><button type="button" className={styles.addNewBtn} id={styles.firstBtn}>ADD NEW BLOG</button></Link>
-            <div className={styles.contentWrapper}>
-                <div className={styles.blogWrapper}>
-                    {blogPosts.slice(page, page + 1).map((post) => (
-                        <MainBlog key={post.frontmatter.id} post={post} />
-                    ))}
-                    <div className={styles.miniBlogsWrapper}>
-                        {blogPosts.slice(page + 1, page + numberOfBlogsPerPage).map((post) => (
-                            <Blog key={post.frontmatter.id} post={post} />
+            <div className={styles.centerOfPage}> 
+                <Link href="/blog/addNewBlog"><button type="button" className={styles.addNewBtn} id={styles.firstBtn}>ADD NEW BLOG</button></Link>
+                <div className={styles.contentWrapper}>
+                    <div className={styles.blogWrapper}>
+                        {blogPosts.slice(page, page + 1).map((post) => (
+                            <MainBlog key={post.frontmatter.id} post={post} />
                         ))}
+                        <div className={styles.miniBlogsWrapper}>
+                            {blogPosts.slice(page + 1, page + numberOfBlogsPerPage).map((post) => (
+                                <Blog key={post.frontmatter.id} post={post} />
+                            ))}
+                        </div>
                     </div>
-                </div>
-                    <div className={styles.pageWrapper}>
-                        <Image
-                            id={styles.arrow}
-                            className={page == indexOfFirstBlog ? styles.hiddenArrow : null}
-                            width={196}
-                            height={220}
-                            src="/blogpics/Arrow 2.svg"
-                            alt="next page arrow"
-                            onClick={() => page > 0 ? setPage(page - numberOfBlogsPerPage) : setPage(0)}
-                        />
-                        <div className={styles.pageNum} id={page == indexOfFirstBlog ? styles.currentPage : styles.notCurrentPage} onClick={() => setPage(indexOfFirstBlog)}>1</div>
-                        <div className={styles.pageNum} id={page == (indexOfFirstBlog + numberOfBlogsPerPage) ? styles.currentPage : styles.notCurrentPage} onClick={() => setPage(indexOfFirstBlog + numberOfBlogsPerPage)}>2</div>
-                        <div className={styles.pageNum} id={page == (indexOfFirstBlog + numberOfBlogsPerPage * 2) ? styles.currentPage : styles.notCurrentPage} onClick={() => setPage(indexOfFirstBlog + numberOfBlogsPerPage * 2)}>3</div>
-                        <Image
-                            id={styles.arrow}
-                            className={page == (indexOfFirstBlog + numberOfBlogsPerPage * 2) ? styles.hiddenArrow : null}
-                            width={196}
-                            height={220}
-                            src="/blogpics/Arrow 1.svg"
-                            alt="next page arrow"
-                            onClick={() => page == (indexOfFirstBlog + numberOfBlogsPerPage * 2) ? setPage(indexOfFirstBlog + numberOfBlogsPerPage * 2) : setPage(page + numberOfBlogsPerPage)}
-                        />
-                    </div>
-                    </div>
+                        <div className={styles.pageWrapper}>
+                            <Image
+                                id={styles.arrow}
+                                className={page == indexOfFirstBlog ? styles.hiddenArrow : null}
+                                width={196}
+                                height={220}
+                                src="/blogpics/Arrow 2.svg"
+                                alt="next page arrow"
+                                onClick={() => page > 0 ? setPage(page - numberOfBlogsPerPage) : setPage(0)}
+                            />
+                            <div className={styles.pageNum} id={page == indexOfFirstBlog ? styles.currentPage : styles.notCurrentPage} onClick={() => setPage(indexOfFirstBlog)}>1</div>
+                            <div className={styles.pageNum} id={page == (indexOfFirstBlog + numberOfBlogsPerPage) ? styles.currentPage : styles.notCurrentPage} onClick={() => setPage(indexOfFirstBlog + numberOfBlogsPerPage)}>2</div>
+                            <div className={styles.pageNum} id={page == (indexOfFirstBlog + numberOfBlogsPerPage * 2) ? styles.currentPage : styles.notCurrentPage} onClick={() => setPage(indexOfFirstBlog + numberOfBlogsPerPage * 2)}>3</div>
+                            <Image
+                                id={styles.arrow}
+                                className={page == (indexOfFirstBlog + numberOfBlogsPerPage * 2) ? styles.hiddenArrow : null}
+                                width={196}
+                                height={220}
+                                src="/blogpics/Arrow 1.svg"
+                                alt="next page arrow"
+                                onClick={() => page == (indexOfFirstBlog + numberOfBlogsPerPage * 2) ? setPage(indexOfFirstBlog + numberOfBlogsPerPage * 2) : setPage(page + numberOfBlogsPerPage)}
+                            />
+                        </div>
+                        </div>
+            </div>
             <Footer />
         </>
     );
