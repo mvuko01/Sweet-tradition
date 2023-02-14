c503e29d1f4f22030c7ebf3bd02716c778ec96d9
mvuko01 <mvuko01@fesb.hr>
2023-02-14 15:19:36 +0100

    Second home page banner completed

--- candyshop/components/StoryBanner.js
+++ candyshop/components/StoryBanner.js
@@ -1,24 +1,35 @@
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/StoryBanner.module.css'
 import Link from 'next/link';
 
 const StoryBanner = () => {
     return (
-        <div className={styles.storyBannerWrapper}>
-            <Image
-                    src={'/story banner2.svg'}
-                    alt="Banner"
-                    width={1800}
-                    height={384}
-                    className={styles.storyBanner}
-                />
-                <div className={styles.textWrapper}>
-                    <p className={styles.shopOurSO}>SHOP OUR SELECTION OF <span className={styles.bulk}>BULK CANDY, GUMMIES, CHOCOLATES</span> AND MORE</p>
-                    <p className={styles.stock}>Stock up on sweet treats today!</p>
-                
-                    <Link href={"/candy"} passHref className={styles.btnLink}><button type="button" className={styles.buttonStoryBanner}>SHOP NOW</button></Link>
+        <div className={styles.bannerContainer}>
+            <div className={styles.banner}>
+                <div className={styles.textFirstBanner}>
+                    <p className={styles.shopAt}>SHOP OUR SELECTION OF BULK CANDY, GUMMIES, CHOCOLATES AND MORE</p>
+                    <p className={styles.treat}>Stock up on sweet treats today!</p>
+                    <Link href={"/candy"} passHref>
+                        <button type="button">SHOP NOW</button>
+                    </Link>
                 </div>
+            </div>
         </div>
+        // <div className={styles.storyBannerWrapper}>
+        //     <Image
+        //             src={'/story banner2.svg'}
+        //             alt="Banner"
+        //             width={1800}
+        //             height={384}
+        //             className={styles.storyBanner}
+        //         />
+        //         <div className={styles.textWrapper}>
+        //             <p className={styles.shopOurSO}>SHOP OUR SELECTION OF <span className={styles.bulk}>BULK CANDY, GUMMIES, CHOCOLATES</span> AND MORE</p>
+        //             <p className={styles.stock}>Stock up on sweet treats today!</p>
+                
+        //             <Link href={"/candy"} passHref className={styles.btnLink}><button type="button" className={styles.buttonStoryBanner}>SHOP NOW</button></Link>
+        //         </div>
+        // </div>
     );
 };
 
