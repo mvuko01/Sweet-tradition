1148bae19ccc547b27c728080314e79de113bec7
mvuko01 <mvuko01@fesb.hr>
2023-02-14 14:43:56 +0100

    Fixed first banner

--- candyshop/components/FirstBanner.js
+++ candyshop/components/FirstBanner.js
@@ -1,31 +1,22 @@
 import Image from 'next/image';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/FirstBanner.module.css'
 import Link from 'next/link';
 
 const FirstBanner = () => {
     return (
         <>
-        <div className={styles.firstBannerWrapper}>
-            <Image
-                    src={'/banner image.svg'}
-                    alt="Banner"
-                    width={1}
-                    height={1}
-                    className={styles.firstBannnerImage}
-                />
+        <div className={styles.bannerContainer}>
+            <div className={styles.banner}>
                 <div className={styles.textFirstBanner}>
-                    <div className={styles.shop}>
-                        <div className={styles.shopAtWrapper}>
-                            <p className={styles.shopAt}>SHOP AT</p>
-                            <span className={styles.sweet}>SWEET TRADITION!</span>
-                        </div>
-                    </div>
+                    <p className={styles.shopAt}>SHOP AT SWEET TRADITION!</p>
                     <p className={styles.treat}>TREAT YOURSELF TO SWEETNESS</p>
                     <Link href="/candy/candy">
-                    <button type='button'>BUY NOW</button>
+                        <button type='button'>BUY NOW</button>
                     </Link>
                 </div>
+            </div>
         </div>
+        
         </>
     );
 };
