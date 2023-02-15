65c94fb06d9233fbc132abd384547bc0fbeae18b
mvuko01 <mvuko01@fesb.hr>
2023-02-16 00:50:11 +0100

    Blog page responsive up to mobile

--- candyshop/components/Blog.js
+++ candyshop/components/Blog.js
@@ -5,25 +5,25 @@ import { limitWords } from '../helpers';
 const Blog = ({ post }) => {
     return (
         <>
-        <div className={styles.miniBlogWrapper}>
+            <div className={styles.miniBlogWrapper}>
                 <Image
-                className={styles.blogPics}
-                width={196}
-                height={220}
-                src={post.frontmatter.picture}
-                alt="blog wrapper"
-            />
-            <div className={styles.miniBlogContentWrapper}>
-                <p className={styles.title}>{post.frontmatter.title}</p>
-               <div className={styles.textBtnWrapperMini}>  
+                    className={styles.sidePics}
+                    width={196}
+                    height={220}
+                    src={post.frontmatter.picture}
+                    alt="blog wrapper"
+                />
+                <div className={styles.miniBlogContentWrapper}>
+                    <p className={styles.title}>{post.frontmatter.title}</p>
+
                     <p className={styles.ctaText}>{limitWords(post.frontmatter.cta)}</p>
-                    <Link href={`../blog/${post.slug}`} className={styles.miniLink}>
+                    <Link href={`../blog/${post.slug}`} className={styles.link}>
                         <button type="button" className={styles.miniBtn}>READ MORE</button>
                     </Link>
-                 </div> 
-            </div> 
-        </div>
-            </>
+
+                </div>
+            </div>
+        </>
     );
 };
 
--- candyshop/components/MainBlog.js
+++ candyshop/components/MainBlog.js
@@ -5,27 +5,25 @@ import { limitWords } from '../helpers';
 
 const MainBlog = ({ post }) => {
     return (
-            <>
+        <>
             <div className={styles.mainBlogWrapper}>
-                
+
                 <Image
-                className={styles.mainPic}
-                width={405}
-                height={440}
-                src={post.frontmatter.picture}
-                alt="profile image"
-            />
-            <div className={styles.mainBlogContentWrapper}>
-                <p className={styles.mainTitle}>{post.frontmatter.title}</p>
-                <div className={styles.textBtnWrapper}>
+                    className={styles.mainPic}
+                    width={405}
+                    height={440}
+                    src={post.frontmatter.picture}
+                    alt="profile image"
+                />
+                <div className={styles.mainBlogContentWrapper}>
+                <p className={styles.title}>{post.frontmatter.title}</p>
                 <p className={styles.ctaText}>{limitWords(post.frontmatter.cta)}</p>
-                <Link href={`../blog/${post.slug}`} className={styles.mainLink}>
+                <Link href={`../blog/${post.slug}`} className={styles.link}>
                     <button type="button">READ MORE</button>
                 </Link>
+                </div>
             </div>
-                </div>
-                </div>
-            </>
+        </>
     );
 };
 
