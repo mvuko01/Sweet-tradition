71b8e7c2b9a19fb178c201df0fd5323cf1e3af31
“kbtomic” <klara.tomic69@gmail.com>
2023-03-16 20:52:48 +0100

    Added return keyword into registerUser.ts

--- candyshop/pages/api/registerUser.ts
+++ candyshop/pages/api/registerUser.ts
@@ -33,12 +33,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       expiresIn: '1h',
     });
 
-    res.status(200).json({ token });
+    return res.status(200).json({ token });
 
     //res.status(201).json({ message: 'User created', user });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Sorry, something went wrong!' });
+    return res.status(500).json({ message: 'Sorry, something went wrong!' });
   } finally {
     await prisma.$disconnect();
   }
