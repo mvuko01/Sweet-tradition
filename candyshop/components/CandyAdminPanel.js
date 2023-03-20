af64d0c46b8aec720a0477ebf72c581ea482ccd6
“kbtomic” <klara.tomic69@gmail.com>
2023-03-20 11:48:49 +0100

    Added dashed border below product in admin panel

--- candyshop/components/CandyAdminPanel.js
+++ candyshop/components/CandyAdminPanel.js
@@ -4,22 +4,24 @@ import styles from '../styles/AdminPanel.module.css';
 const CandyAdminPanel = ({product}) => {
     return (
         <>
-        <Image
-                width={196}
-                height={220}
-                src={`/productPics/${product.picture_paths[0]}`}
-                alt="next page arrow"
-            />
-        <p className={styles.infoLabels}>NAME</p>
-            <p>{product.name}</p>
-        <p className={styles.infoLabels}>PRICE</p>
-            <p>{product.price}</p>
-        <p className={styles.infoLabels}>QUANTITY</p>
-            <p>{product.quantity}</p>
-        <p className={styles.infoLabels}>CATEGORY</p>
-            <p>{product.category.name}</p>
-        <button>EDIT</button>
-        <button>DELETE</button>
+        <div className={styles.productContainer}>
+            <Image
+                    width={196}
+                    height={220}
+                    src={`/productPics/${product.picture_paths[0]}`}
+                    alt="next page arrow"
+                />
+            <p className={styles.infoLabels}>NAME</p>
+                <p>{product.name}</p>
+            <p className={styles.infoLabels}>PRICE</p>
+                <p>{product.price}</p>
+            <p className={styles.infoLabels}>QUANTITY</p>
+                <p>{product.quantity}</p>
+            <p className={styles.infoLabels}>CATEGORY</p>
+                <p>{product.category.name}</p>
+            <button>EDIT</button>
+            <button>DELETE</button>
+        </div>
         </>
     );
 }
