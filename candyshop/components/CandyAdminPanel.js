1b731af8e582041b9b01b9abc281834db805364e
“kbtomic” <klara.tomic69@gmail.com>
2023-03-20 11:26:45 +0100

    Reading candies from db

--- candyshop/components/CandyAdminPanel.js
+++ candyshop/components/CandyAdminPanel.js
@@ -1,18 +1,22 @@
 import Image from "next/image";
 
-const CandyAdminPanel = () => {
+const CandyAdminPanel = ({product}) => {
     return (
         <>
         <Image
                 width={196}
                 height={220}
-                src="/productPics/berlingots.svg"
+                src={`/productPics/${product.picture_paths[0]}`}
                 alt="next page arrow"
             />
         <p>NAME</p>
+            <p>{product.name}</p>
         <p>PRICE</p>
+            <p>{product.price}</p>
         <p>QUANTITY</p>
+            <p>{product.quantity}</p>
         <p>CATEGORY</p>
+            <p>{product.category.name}</p>
         <button>EDIT</button>
         <button>DELETE</button>
         </>
--- candyshop/pages/adminPanel.js
+++ candyshop/pages/adminPanel.js
@@ -2,13 +2,43 @@ import Header2 from '../components/Header2';
 import Footer from '../components/Footer';
 import styles from '../styles/adminPanel.module.css';
 import CandyAdminPanel from '../components/CandyAdminPanel';
+import prisma from '../prisma/client';
 
-const AdminPanel = () => {
+export async function getServerSideProps() {
+    try {
+        const products = await prisma.candy.findMany({
+            include:{
+                category: {
+                  select:{
+                    name: true,
+                  }
+                }
+              }
+        })
+        return {
+            props: {
+              products: JSON.parse(JSON.stringify(products)),
+            },
+        }
+    } catch (error) {
+        return {
+            props: {
+              products: [],
+            },
+        }
+    }
+}
+const AdminPanel = (props) => {
+    const products = props.products;
     return (
         <>
         <Header2 />
         <h1 className={styles.heading}>ADMIN PANEL</h1>
-        <CandyAdminPanel />
+        {products.map((product) => {
+            return (
+                <CandyAdminPanel key={product.id} product={product} />
+            )
+        })}
         <Footer />
         </>
     )
