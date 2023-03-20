67947a5bb73453ba09ae5daef176954f4c6c14cc
“kbtomic” <klara.tomic69@gmail.com>
2023-03-20 11:31:55 +0100

    Added page numbering to admin panel

--- candyshop/pages/adminPanel.js
+++ candyshop/pages/adminPanel.js
@@ -2,6 +2,8 @@ import Header2 from '../components/Header2';
 import Footer from '../components/Footer';
 import styles from '../styles/adminPanel.module.css';
 import CandyAdminPanel from '../components/CandyAdminPanel';
+import PageNumber from '../components/PageNumbers';
+import { useState } from 'react';
 import prisma from '../prisma/client';
 
 export async function getServerSideProps() {
@@ -30,15 +32,46 @@ export async function getServerSideProps() {
 }
 const AdminPanel = (props) => {
     const products = props.products;
+    const productsPerPage = 12;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentArray, setCurrentArray] = useState(products);
+    const numberOfPages = Math.ceil(currentArray.length / productsPerPage);
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = currentArray.slice(indexOfFirstProduct, indexOfLastProduct);
+
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    };
+
+    const pageNumbers = [];
+    for (let i = 1; i <= numberOfPages; i++) {
+        pageNumbers.push(i);
+    }
+
+    let firstPageNumber, lastPageNumber;
+    if (currentPage <= 2) {
+        firstPageNumber = 0;
+        lastPageNumber = 2;
+    } else if (currentPage >= numberOfPages - 1) {
+        firstPageNumber = numberOfPages - 3;
+        lastPageNumber = numberOfPages - 1;
+    } else {
+        firstPageNumber = currentPage - 2;
+        lastPageNumber = currentPage;
+    }
+    const visiblePageNumbers = pageNumbers.slice(firstPageNumber, lastPageNumber + 1);
     return (
         <>
         <Header2 />
         <h1 className={styles.heading}>ADMIN PANEL</h1>
-        {products.map((product) => {
+        <PageNumber currentPage={currentPage} handlePageChange={handlePageChange} visiblePageNumbers={visiblePageNumbers} numberOfPages={numberOfPages}/>
+        {currentProducts.map((product) => {
             return (
                 <CandyAdminPanel key={product.id} product={product} />
             )
         })}
+        <PageNumber currentPage={currentPage} handlePageChange={handlePageChange} visiblePageNumbers={visiblePageNumbers} numberOfPages={numberOfPages}/>
         <Footer />
         </>
     )
